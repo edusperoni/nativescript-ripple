ffcf65f0807ff92ed107dfb85bf6e01dea9f67f6
Morten Anton Bach Sjøgren <msn@nota.dk>
2016-12-17 00:22:13 +0100

    chor: Adds basic support for iOS

--- ripple.android.ts
+++ ripple.android.ts
@@ -6,56 +6,17 @@
 * Open Source Lib : https://github.com/balysv/material-ripple
 *************************************************************************************/
 
-import { DependencyObservable, PropertyChangeData, Property, PropertyMetadataSettings } from "ui/core/dependency-observable";
-import { ContentView } from "ui/content-view";
-import { View } from "ui/core/view";
-import { PropertyMetadata } from "ui/core/proxy";
+import * as common from './ripple-common';
+
 import { Color } from "color";
-import {isAndroid} from "platform";
 
+declare var android: any;
+declare var java: any;
+declare var com: any;
 
-// on Android we explicitly set propertySettings to None because android will invalidate its layout (skip unnecessary native call).
-let AffectsLayout = isAndroid ? PropertyMetadataSettings.None : PropertyMetadataSettings.AffectsLayout;
-
-
-declare var android, java, com: any;
-
-
-const rippleColorProperty = new Property(
-    "rippleColor",
-    "Ripple",
-    new PropertyMetadata("", AffectsLayout)
-);
-
-const rippleAlphaProperty = new Property(
-    "rippleAlpha",
-    "Ripple",
-    new PropertyMetadata("", AffectsLayout)
-);
-
-const rippleDurationProperty = new Property(
-    "rippleDuration",
-    "Ripple",
-    new PropertyMetadata("", AffectsLayout)
-);
-
-const fadeDurationProperty = new Property(
-    "fadeDuration",
-    "Ripple",
-    new PropertyMetadata("", AffectsLayout)
-);
-
-export class Ripple extends ContentView {
-    private _android: com.balysv.materialripple.MaterialRippleLayout;
+export class Ripple extends common.Ripple {
+    private _android: any; /*com.balysv.materialripple.MaterialRippleLayout;*/
     private _androidViewId: number;
-    public static rippleColorProperty = rippleColorProperty;
-    public static rippleAlphaProperty = rippleAlphaProperty;
-    public static rippleDurationProperty = rippleDurationProperty;
-    public static fadeDurationProperty = fadeDurationProperty;
-
-    constructor() {
-        super();
-    }
 
     get android(): any {
         return this._android;
@@ -65,36 +26,7 @@ export class Ripple extends ContentView {
         return this._android;
     }
 
-    get rippleColor(): string {
-        return this._getValue(Ripple.rippleColorProperty);
-    }
-    set rippleColor(value: string) {
-        this._setValue(Ripple.rippleColorProperty, value);
-    }
-
-    get rippleAlpha(): any {
-        return this._getValue(Ripple.rippleAlphaProperty);
-    }
-    set rippleAlpha(value: any) {
-        this._setValue(Ripple.rippleAlphaProperty, value);
-    }
-
-    get rippleDuration(): any {
-        return this._getValue(Ripple.rippleDurationProperty);
-    }
-    set rippleDuration(value: any) {
-        this._setValue(Ripple.rippleDurationProperty, value);
-    }
-
-    get fadeDuration(): any {
-        return this._getValue(Ripple.fadeDurationProperty);
-    }
-    set fadeDuration(value: any) {
-        this._setValue(Ripple.fadeDurationProperty, value);
-    }
-
     public _createUI() {
-
         this._android = new com.balysv.materialripple.MaterialRippleLayout(this._context);
 
         this._android.setRippleOverlay(true);
@@ -122,27 +54,9 @@ export class Ripple extends ContentView {
         //     console.log(this.fadeDuration);
         //     // this._android.setRippleFadeDuration(this.fadeDuration);
         // }
-
     }
 
-
-    public _addChildFromBuilder(name: string, value: any) {
-        // Copy inheirtable style property values
-        //var originalColor = value.style.color || null;
-
-        if (value instanceof View) {
-            this.content = value;
-        }
-
-        // Reset inheritable style property values as we do not want those to be inherited
-        //value.style.color = originalColor;
-    }
-
-
-
-    public performRipple() {
+    public performRipple(x: number, y: number) {
         this._android.performRipple();
     }
-
-
 }

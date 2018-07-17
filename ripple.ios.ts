4081ba7d4ed64ea1c7b2029a6951ed07dbc9d500
Shay Ben David <shay@prospera.ag>
2018-07-17 10:40:14 +0300

    - ios: fix compilation error - typescript: skip lib check

--- ripple.ios.ts
+++ ripple.ios.ts
@@ -1,9 +1,9 @@
 /***************************************************************************************
-* Made for the {N} community by Brad Martin @BradWayneMartin
-* https://twitter.com/BradWayneMartin
-* https://github.com/bradmartin
-* http://bradmartin.net
-*************************************************************************************/
+ * Made for the {N} community by Brad Martin @BradWayneMartin
+ * https://twitter.com/BradWayneMartin
+ * https://github.com/bradmartin
+ * http://bradmartin.net
+ *************************************************************************************/
 
 import * as common from "./ripple-common";
 import { View } from "tns-core-modules/ui/core/view";
@@ -31,15 +31,17 @@ export class Ripple extends common.Ripple {
 
     const size = this.content.getActualSize();
     const radius = Math.min(
-      Math.min(size.height, size.width) / scale * 0.8,
+      (Math.min(size.height, size.width) / scale) * 0.8,
       60
     );
     const ripple = UIView.alloc().initWithFrame(
       CGRectMake(0, 0, radius, radius)
     );
     ripple.layer.cornerRadius = radius * 0.5;
-    ripple.backgroundColor = (this.content.backgroundColor ||
-      new Color("#000000")).ios;
+    ripple.backgroundColor = (this.content.backgroundColor instanceof Color
+      ? this.content.backgroundColor
+      : new Color(this.content.backgroundColor || "#000000")
+    ).ios;
     ripple.alpha = 1.0;
     nativeView.insertSubviewAtIndex(ripple, 0);
     ripple.center = CGPointMake(x || 0, y || 0);

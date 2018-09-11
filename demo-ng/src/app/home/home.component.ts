d8925c9d0afc7c92f31f4cc0f88e628a62f2dd44
Eduardo Speroni <edusperoni@gmail.com>
2018-09-11 10:56:23 -0300

    angular demo

---
+++ demo-ng/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "Home",
+    moduleId: module.id,
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"]
+})
+export class HomeComponent implements OnInit {
+
+    constructor() {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        // Init your component properties here.
+    }
+
+    dummy(): void {
+
+    }
+    tapEvent() {
+        alert('Tap Event Works too');
+    }
+}
---
+++ demo-ng/src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptCommonModule } from "nativescript-angular/common";
+
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
+import { NgRippleModule } from "nativescript-ng-ripple";
+
+@NgModule({
+    imports: [
+        NativeScriptCommonModule,
+        HomeRoutingModule,
+        NgRippleModule
+    ],
+    declarations: [
+        HomeComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class HomeModule { }

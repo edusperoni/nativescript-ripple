2753b857fd5b4b5f6726669e9830a04070e3368c
Eduardo Speroni <edusperoni@gmail.com>
2019-04-08 01:33:38 -0300

    cleanup directive

--- src/angular/ng-ripple.directive.ts
+++ src/angular/ng-ripple.directive.ts
@@ -1,23 +1,5 @@
-import { Directive, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, SimpleChange } from '@angular/core';
-import { Color } from 'tns-core-modules/color';
-import * as platform from 'tns-core-modules/platform';
-import { View } from 'tns-core-modules/ui/core/view';
-import { addWeakEventListener, removeWeakEventListener } from "tns-core-modules/ui/core/weak-event-listener";
-import { GestureTypes, TouchGestureEventData } from 'tns-core-modules/ui/gestures/gestures';
-import { Length } from 'tns-core-modules/ui/styling/style-properties';
+import { Directive, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { Ripple } from '../lib/ripple';
-
-declare const android: any;
-declare const java: any;
-declare const Array: any;
-declare var CGAffineTransformMakeScale: any;
-declare var CGPointMake: any;
-declare var CGRectMake: any;
-declare var UIView: any;
-declare var UIViewAnimationOptionCurveEaseOut: any;
-
-const MARSHMALLOW = 23;
-const LOLLIPOP = 21;
 
 @Directive({ selector: '[ripple]' })
 export class NativeRippleDirective implements OnInit, OnChanges, OnDestroy {

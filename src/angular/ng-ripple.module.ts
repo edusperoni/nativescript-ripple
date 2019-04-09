1a8f1780830ba5a95fc79de8719f9b78de5964c9
Eduardo Speroni <edusperoni@gmail.com>
2019-04-09 00:17:54 -0300

    register Ripple

--- src/angular/ng-ripple.module.ts
+++ src/angular/ng-ripple.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { NativeRippleDirective } from './ng-ripple.directive';
+import { registerElement } from "nativescript-angular/element-registry";
+import { RippleView } from "../ripple-view";
+
+registerElement("Ripple", () => RippleView);
 
 @NgModule({
   imports: [],

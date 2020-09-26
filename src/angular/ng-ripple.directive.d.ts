128ec567eda369391137cdf918d1b4570bcaffad
Eduardo Speroni <edusperoni@gmail.com>
2020-09-26 17:06:56 -0300

    chore: disable ivy (let ngcc handle it)

--- src/angular/ng-ripple.directive.d.ts
+++ src/angular/ng-ripple.directive.d.ts
@@ -1,5 +1,4 @@
 import { ElementRef, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
-import * as i0 from "@angular/core";
 export declare class NativeRippleDirective implements OnInit, OnChanges, OnDestroy {
     private el;
     ripple?: string;
@@ -11,6 +10,4 @@ export declare class NativeRippleDirective implements OnInit, OnChanges, OnDestr
     ngOnInit(): void;
     ngOnDestroy(): void;
     ngOnChanges(changes: SimpleChanges): void;
-    static ɵfac: i0.ɵɵFactoryDef<NativeRippleDirective, never>;
-    static ɵdir: i0.ɵɵDirectiveDefWithMeta<NativeRippleDirective, "[ripple]", never, { "ripple": "ripple"; "rippleColor": "rippleColor"; "rippleColorAlpha": "rippleColorAlpha"; "rippleLayer": "rippleLayer"; }, {}, never>;
 }
